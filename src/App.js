a57c8a2609551f1d890cdb68c963d0e32ca2fdb5
Allan Mallya <mallya.allan@gmail.com>
2018-09-18 13:08:29 +0300

    removed spaces

--- src/App.js
+++ src/App.js
@@ -32,9 +32,7 @@ class Playlist extends Component{
   render() {
     return(
         <div style={{...defaultStyle, width: '25%', display: 'inline-block'}}>
-        
           <img/>
-          
             <h3> Playlist Name</h3>
             <ul><li>Song 1</li><li>Song 2</li><li>Song 3</li><li>Song 4</li></ul>
         </div>
